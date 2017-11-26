1d35c7323aae54a646d9967fd6cc666606fb8108
Sebastian Manzano <samanzanom@gmail.com>
2017-11-26 15:14:26 -0300

    first commit

---
+++ vistas/plantilla.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+	<meta charset="UTF-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+	<meta name="title" content="Tienda Virtual">
+
+	<meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam accusantium enim esse eos officiis sit officia">
+
+	<meta name="keyword" content="Lorem ipsum, dolor sit amet, consectetur, adipisicing, elit, Quisquam, accusantium, enim, esse">
+
+	<title>Tienda Virtual</title>
+
+	<link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+
+	<link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu|Ubuntu+Condensed" rel="stylesheet">
+
+	<link rel="stylesheet" href="vistas/css/plantilla.css">
+
+	<link rel="stylesheet" href="vistas/css/cabezote.css">
+
+	<script type="text/javascript" src="vistas/js/plugins/jquery.min.js"></script>
+
+	<script type="text/javascript" src="vistas/js/plugins/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+	
+	<?php
+		/*=============================================
+		=            Cabezote			              =
+		=============================================*/
+		
+		include "modulos/cabezote.php";
+
+	?>
+	
+
+	
+
+</body>
+</html>
